6e58cf95e1aca925c13e46c11abb5a8691bf3e02
Pedro Pinho <ptfpinho@hotmail.com>
2023-03-17 12:41:39 +0100

    update pg client package

--- packages/rocket-pg/src/client.ts
+++ packages/rocket-pg/src/client.ts
@@ -1,23 +1,8 @@
-import * as net from 'net'
-import { EventEmitter } from 'events';
-
-// shift protocol sign bits
-const PG_PROTOCOL_VERSION = 3 << 16;
-
-interface PostgresConfig {
-  host: string;
-  port: number;
-  user: string;
-  password: string;
-  database: string;
-}
-
-enum ConnectionState {
-  DISCONNECTED = 'DISCONNECTED',
-  CONNECTING = 'CONNECTING',
-  CONNECTED = 'CONNECTED',
-  ERROR = 'ERROR',
-}
+import * as net from "net";
+import { EventEmitter } from "events";
+import { PG_PROTOCOL_VERSION } from "./consts";
+import { ConnectionState } from "./enums";
+import { PostgresConfig } from "./interfaces";
 
 export class RocketPGClient extends EventEmitter {
   private config: PostgresConfig;
@@ -28,27 +13,29 @@ export class RocketPGClient extends EventEmitter {
   constructor(config: PostgresConfig) {
     super();
     this.config = config;
-    this.buffer = Buffer.alloc(0)
+    this.buffer = Buffer.alloc(0);
   }
 
   connect(): Promise<void> {
     if (this.state !== ConnectionState.DISCONNECTED) {
-      return Promise.reject(new Error('Client is already connected or connecting.'));
+      return Promise.reject(
+        new Error("Client is already connected or connecting.")
+      );
     }
     this.state = ConnectionState.CONNECTING;
     this.socket = net.connect(this.config.port, this.config.host);
-    this.socket.on('connect', () => this.handleConnect());
-    this.socket.on('error', (err) => this.handleError(err));
-    this.socket.on('close', () => this.handleClose());
-    this.socket.on('data', (data) => this.handleData(data));
+    this.socket.on("connect", () => this.handleConnect());
+    this.socket.on("error", (err) => this.handleError(err));
+    this.socket.on("close", () => this.handleClose());
+    this.socket.on("data", (data) => this.handleData(data));
     return new Promise((resolve) => {
-      this.once('connect', () => resolve());
+      this.once("connect", () => resolve());
     });
   }
 
   disconnect(): Promise<void> {
     if (this.state !== ConnectionState.CONNECTED) {
-      return Promise.reject(new Error('Client is not connected.'));
+      return Promise.reject(new Error("Client is not connected."));
     }
     this.socket?.destroy();
     this.state = ConnectionState.DISCONNECTED;
@@ -57,19 +44,19 @@ export class RocketPGClient extends EventEmitter {
 
   private handleConnect() {
     this.state = ConnectionState.CONNECTED;
-    this.emit('connect');
+    this.emit("connect");
     this.sendStartupMessage();
   }
 
   private handleError(err: Error) {
     this.state = ConnectionState.ERROR;
-    this.emit('error', err);
+    this.emit("error", err);
   }
 
   private handleClose() {
     if (this.state !== ConnectionState.DISCONNECTED) {
       this.state = ConnectionState.DISCONNECTED;
-      this.emit('disconnect');
+      this.emit("disconnect");
     }
   }
 
@@ -86,16 +73,12 @@ export class RocketPGClient extends EventEmitter {
             this.handleAuthenticationOk(message);
             break;
           case msgType.ErrorResponse:
-
             break;
           case msgType.RowDescription:
-
             break;
           case msgType.DataRow:
-
             break;
           case msgType.CommandComplete:
-
             break;
           default:
             throw new Error(`Unknown message type: ${msgType}`);
@@ -110,13 +93,11 @@ export class RocketPGClient extends EventEmitter {
     // - https://www.postgresql.org/docs/current/protocol-message-formats.html
     // The AuthenticationOk message is a single-byte message with the value 0.
     if (message.length !== 1 || message[0] !== 0) {
-      throw new Error('Invalid AuthenticationOk message');
+      throw new Error("Invalid AuthenticationOk message");
     }
     // Authentication successful, move on to the next step in the startup process.
     this.sendStartupMessage();
   }
-
-
 
   public query(data: any) {
     throw new Error();
@@ -129,16 +110,18 @@ export class RocketPGClient extends EventEmitter {
   private sendStartupMessage() {
     const user = this.config.user;
     const database = this.config.database;
-    const msg = Buffer.alloc(4 + 4 + (user.length + 1) + (database.length + 1) + 1);
+    const msg = Buffer.alloc(
+      4 + 4 + (user.length + 1) + (database.length + 1) + 1
+    );
     let pos = 0;
     msg.writeInt32BE(msg.length);
     pos += 4;
     msg.writeInt32BE(PG_PROTOCOL_VERSION);
     pos += 4;
-    pos += msg.write('user', pos, 'ascii') + 1;
-    pos += msg.write(user, pos, 'ascii') + 1;
-    pos += msg.write('database', pos, 'ascii') + 1;
-    pos += msg.write(database, pos, 'ascii') + 1;
+    pos += msg.write("user", pos, "ascii") + 1;
+    pos += msg.write(user, pos, "ascii") + 1;
+    pos += msg.write("database", pos, "ascii") + 1;
+    pos += msg.write(database, pos, "ascii") + 1;
     msg.writeInt8(0);
     this.socket?.write(msg);
   }
---
+++ packages/rocket-pg/src/consts.ts
@@ -0,0 +1,2 @@
+// shift protocol sign bits
+export const PG_PROTOCOL_VERSION = 3 << 16;
